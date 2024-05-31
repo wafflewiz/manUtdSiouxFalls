55f5ec8a52237e5f7430adde8345bca90838b0a7
Rami Aljanaby <ramialj94@yahoo.com>
2024-05-30 21:56:57 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,7 @@ import {
 
 
 function App() {
-
-
+  
   return (
     <div className="bg-red-500 text-white min-h-screen p-4">
     <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-yellow-500">
